1824c6301e824969b2e98f642351124122a6f762
ytnd0928 <yetianxiongfei@gmail.com>
2023-12-08 14:48:45 +0900

    [fix]remove unused code

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,5 @@
 <style></style>
 
 <script setup>
-import Header from "./components/Header.vue"; // ヘッダーコンポーネントのパスを指定
-// import Home from "./components/Home.vue";
-import { ref, onMounted } from "vue";
-
-const showOverlay = ref(true);
-
-onMounted(() => {
-  // アクセス時に3秒後にフェードアウトを非表示にする
-  setTimeout(() => {
-    showOverlay.value = false;
-  }, 3000); // ミリ秒単位の時間を指定
-});
+import Header from "./components/Header.vue";
 </script>
